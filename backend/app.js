4a26bb6863d5adf9eb6519069e4ab0936851e611
Valera-Kondratovich <kondratovichval@yandex.ru>
2023-07-12 15:50:25 +0300

    ПР15

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(requestLogger);
 app.use(cors({
   credentials: true,
-  origin: '*',
 }));
 app.use(cookieSession({
   secret: 'yourSecret',
